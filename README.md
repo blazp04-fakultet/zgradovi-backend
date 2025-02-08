19d6871f27f370ef192e5c64f18d00720c4c7784
blazp04 <blaz.peric4@gmail.com>
2025-02-08 21:26:55 +0100

    copying default backend and changing cors settings

---
+++ README.md
@@ -0,0 +1,5 @@
+# Cities API
+
+- npm i
+- npm start
+- docs url http://localhost:3000/api-docs
---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const cors = require('cors');
+const userRouter = require('./routes/user');
+const cityRouter = require('./routes/city');
+const errorController = require('./controller/error');
+const path = require('path');
+const swaggerUi = require('swagger-ui-express');
+const docsConfig = require('./docs/config');
+
+const app = express();
+
+app.use(
+  cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
+    allowedHeaders: [
+      'Content-Type',
+      'Authorization',
+      'Accept',
+      'Origin',
+      'X-Requested-With',
+      'ngrok-skip-browser-warning',
+    ],
+    credentials: true,
+    optionsSuccessStatus: 200,
+  })
+);
+
+app.use(express.json({ limit: '10kb' }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// ROUTES
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/cities', cityRouter);
+
+// DOCS
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(docsConfig));
+
+app.all('*', (req, res, next) => {
+  next(new Error(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+app.use(errorController);
+
+module.exports = app;
